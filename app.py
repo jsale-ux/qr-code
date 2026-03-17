c9a14b77f70ec35037531afc70f170c571f80d59
jsale-ux <jsale@csumb.edu>
2026-03-16 22:21:45 -0700

    Refactor QR code generation code in app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import streamlit as st
 import qrcode
-from PIL import Image  # allows us to use Image functions from Pillow library
+from PIL import Image
 
-# Optional improvement (not required):
-# st.title("QR Code Generator")
+st.title("QR Code Generator")
 
-data = st.text_input("Enter text or URL", "Python is fun")  # creates an input box; default text is "Python is fun"
-img = qrcode.make(data)  # generate QR code from whatever user enters
-img.save('MyQRCode1.jpg')  # save QR code as image file
+data = st.text_input("Enter text or URL", "Python is fun")
+img = qrcode.make(data)
+img.save("MyQRCode1.jpg")
 img = Image.open("MyQRCode1.jpg")
-# open the saved QR code image
 
 st.image(img)
-# display the QR code in the Streamlit web app
-
